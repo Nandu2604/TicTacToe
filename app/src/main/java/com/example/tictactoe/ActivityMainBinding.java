a43a5479ac77f174c0d9d1c5666540ba1087b480
nandini <dandolanandini@gmail.com>
2023-10-01 20:05:22 +0530

    Final commit

---
+++ app/src/main/java/com/example/tictactoe/ActivityMainBinding.java
@@ -0,0 +1,30 @@
+package com.example.tictactoe;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.WindowDecorActionBar;
+
+public class ActivityMainBinding {
+    public WindowDecorActionBar.TabImpl playerOneName;
+    public WindowDecorActionBar.TabImpl playerTwoName;
+    public View image1;
+    public View image2;
+    public View image3;
+    public View image4;
+    public View image5;
+    public View image6;
+    public View image7;
+    public View image8;
+    public View image9;
+    public View playerOneLayout;
+    public View playerTwoLayout;
+
+    public static ActivityMainBinding inflate(LayoutInflater layoutInflater) {
+        return null;
+    }
+
+    public View getRoot() {
+        return null;
+    }
+}
